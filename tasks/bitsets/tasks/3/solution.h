062ccc900679a0bd208894cba258040b599c83f8
mnlyuzina <mnlyuzina@yandex.ru>
2024-01-06 19:16:22 +0300

    bitsets_3

--- tasks/bitsets/tasks/3/solution.h
+++ tasks/bitsets/tasks/3/solution.h
@@ -2,10 +2,26 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef uint32_t Bitset;
+const Bitset VOWELS = 17842449;
 
 int Task() {
-    // write your solution here
+    Bitset word = 0;
+    char input[] = "abbcccccfwfw ee fool dd ";
+    for (size_t i = 0; i <= strlen(input); ++i) {
+        if (input[i] >= 'a' && input[i] <= 'z') {
+            if (((word & (1u << (input[i] - 'a'))) != 0) && ((VOWELS & (1u << (input[i] - 'a'))) != 0)) {
+                printf("Word with repeated vowels exist\n");
+                return 0;
+            } else {
+                word |= (1u << (input[i] - 'a'));
+            }
+        } else {
+            word = 0;
+        }
+    }
+    printf("Word with repeated vowels not exist\n");
     return 0;
 }
